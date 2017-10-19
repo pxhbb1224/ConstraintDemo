26084925c63dfc4a31a41629cc692912cc90e432
huangwei <huangwei@tigerbrokers.com>
2017-10-19 16:25:26 +0800

    MOD 使用测试版测试ConstraintLayout-1.1beta

--- app/src/main/java/com/example/huangwei/constraintdemo/MainActivity.java
+++ app/src/main/java/com/example/huangwei/constraintdemo/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.huangwei.constraintdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    setContentView(R.layout.chain);
+    //final TextView textGone = (TextView) findViewById(R.id.text_will_gone);
+    //findViewById(R.id.text_gone_right).setOnClickListener(new View.OnClickListener() {
+    //  boolean flag = false;
+    //
+    //  @Override
+    //  public void onClick(View v) {
+    //    textGone.setVisibility(flag ? View.VISIBLE : View.GONE);
+    //    flag = !flag;
+    //  }
+    //});
+    //Guideline guideline;
+    //final TextView textGone1 = (TextView) findViewById(R.id.textView4);
+    //findViewById(R.id.text_click_gone_constraint).setOnClickListener(new View.OnClickListener() {
+    //  boolean flag = false;
+    //
+    //  @Override
+    //  public void onClick(View v) {
+    //    textGone1.setVisibility(flag ? View.VISIBLE : View.GONE);
+    //    flag = !flag;
+    //  }
+    //});
   }
 }
